6e84a744f6b85fadca8f0f670c4b5d188aa82bb7
VictorVdi <victor.vedie@telecom-sudparis.eu>
2018-11-27 17:34:09 +0100

    The Final Countdown

--- var/cache/dev/twig/e5/e58c4a9ad7e5781e967bde218ed9fc730e1cbe9517fb9d67674128db19394414.php
+++ var/cache/dev/twig/e5/e58c4a9ad7e5781e967bde218ed9fc730e1cbe9517fb9d67674128db19394414.php
@@ -15,7 +15,7 @@ class __TwigTemplate_6c3c2f7ea93458d7b3b5b73432469264cce484c1c4fa0ec4ee5548d94e2
         $this->parent = $this->loadTemplate("base.html.twig", "back/etape/show.html.twig", 1);
         $this->blocks = array(
             'title' => array($this, 'block_title'),
-            'body' => array($this, 'block_body'),
+            'main' => array($this, 'block_main'),
         );
     }
 
@@ -41,7 +41,7 @@ class __TwigTemplate_6c3c2f7ea93458d7b3b5b73432469264cce484c1c4fa0ec4ee5548d94e2
 
     }
 
-    // line 3
+    // line 2
     public function block_title($context, array $blocks = array())
     {
         $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
@@ -59,65 +59,116 @@ class __TwigTemplate_6c3c2f7ea93458d7b3b5b73432469264cce484c1c4fa0ec4ee5548d94e2
 
     }
 
-    // line 5
-    public function block_body($context, array $blocks = array())
+    // line 3
+    public function block_main($context, array $blocks = array())
     {
         $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
-        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "main"));
 
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
-        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "main"));
 
-        // line 6
-        echo "    <h1>Etape</h1>
+        // line 4
+        echo "<html lang=\"fr\">
 
-    <table class=\"table\">
-        <tbody>
-            <tr>
-                <th>Id</th>
-                <td>";
-        // line 12
-        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["etape"]) || array_key_exists("etape", $context) ? $context["etape"] : (function () { throw new Twig_Error_Runtime('Variable "etape" does not exist.', 12, $this->source); })()), "id", array()), "html", null, true);
-        echo "</td>
-            </tr>
-            <tr>
-                <th>NumeroEtape</th>
-                <td>";
-        // line 16
-        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["etape"]) || array_key_exists("etape", $context) ? $context["etape"] : (function () { throw new Twig_Error_Runtime('Variable "etape" does not exist.', 16, $this->source); })()), "numeroEtape", array()), "html", null, true);
-        echo "</td>
-            </tr>
-            <tr>
-                <th>VilleEtape</th>
-                <td>";
-        // line 20
-        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["etape"]) || array_key_exists("etape", $context) ? $context["etape"] : (function () { throw new Twig_Error_Runtime('Variable "etape" does not exist.', 20, $this->source); })()), "villeEtape", array()), "html", null, true);
-        echo "</td>
-            </tr>
-            <tr>
-                <th>NombreJours</th>
-                <td>";
-        // line 24
-        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["etape"]) || array_key_exists("etape", $context) ? $context["etape"] : (function () { throw new Twig_Error_Runtime('Variable "etape" does not exist.', 24, $this->source); })()), "nombreJours", array()), "html", null, true);
-        echo "</td>
-            </tr>
-        </tbody>
-    </table>
+<head>
 
-    <a href=\"";
-        // line 29
+    <meta charset=\"utf-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
+    <meta name=\"Auteur\" content=\"Dylan Soton & Victor Védie\">
+
+    <!-- Quelques polices d'écritures plus jolies :) -->
+    <link href=\"/css/bootstrap.min.css\" rel=\"stylesheet\">
+    <link href=\"https://fonts.googleapis.com/css?family=Varela+Round\" rel=\"stylesheet\">
+    <link href=\"https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i\" rel=\"stylesheet\">
+
+    <!-- Un Css modifié :) -->
+    <link href=\"../../css/circuit.css\" rel=\"stylesheet\">
+
+</head>
+<body id=\"page-top\">
+
+<!-- NavBar Menu -->
+<nav class=\"navbar navbar-expand-lg navbar-light fixed-top\" id=\"mainNav\">
+    <div class=\"container\">
+        <a href=\"";
+        // line 26
         echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("admin_etape_index");
         echo "\">back to list</a>
 
-    <a href=\"";
-        // line 31
-        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("admin_etape_edit", array("id" => twig_get_attribute($this->env, $this->source, (isset($context["etape"]) || array_key_exists("etape", $context) ? $context["etape"] : (function () { throw new Twig_Error_Runtime('Variable "etape" does not exist.', 31, $this->source); })()), "id", array()))), "html", null, true);
+        <a href=\"";
+        // line 28
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("admin_etape_edit", array("id" => twig_get_attribute($this->env, $this->source, (isset($context["etape"]) || array_key_exists("etape", $context) ? $context["etape"] : (function () { throw new Twig_Error_Runtime('Variable "etape" does not exist.', 28, $this->source); })()), "id", array()))), "html", null, true);
         echo "\">edit</a>
 
+
+    </div>
     ";
-        // line 33
+        // line 32
         echo twig_include($this->env, $context, "back/etape/_delete_form.html.twig");
         echo "
+</nav>
+
+<section id=\"container-table\" class=\"about-section text-center\">
+    <div class=\"container\">
+        <div class=\"row\">
+            <div class=\"col-lg-10 mx-auto\">
+                    <h1>Etape</h1>
+
+                    <table class=\"table\">
+                        <tbody>
+                            <tr>
+                                <th>Id</th>
+                                <td>";
+        // line 45
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["etape"]) || array_key_exists("etape", $context) ? $context["etape"] : (function () { throw new Twig_Error_Runtime('Variable "etape" does not exist.', 45, $this->source); })()), "id", array()), "html", null, true);
+        echo "</td>
+                            </tr>
+                            <tr>
+                                <th>NumeroEtape</th>
+                                <td>";
+        // line 49
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["etape"]) || array_key_exists("etape", $context) ? $context["etape"] : (function () { throw new Twig_Error_Runtime('Variable "etape" does not exist.', 49, $this->source); })()), "numeroEtape", array()), "html", null, true);
+        echo "</td>
+                            </tr>
+                            <tr>
+                                <th>VilleEtape</th>
+                                <td>";
+        // line 53
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["etape"]) || array_key_exists("etape", $context) ? $context["etape"] : (function () { throw new Twig_Error_Runtime('Variable "etape" does not exist.', 53, $this->source); })()), "villeEtape", array()), "html", null, true);
+        echo "</td>
+                            </tr>
+                            <tr>
+                                <th>NombreJours</th>
+                                <td>";
+        // line 57
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["etape"]) || array_key_exists("etape", $context) ? $context["etape"] : (function () { throw new Twig_Error_Runtime('Variable "etape" does not exist.', 57, $this->source); })()), "nombreJours", array()), "html", null, true);
+        echo "</td>
+                            </tr>
+                        </tbody>
+                    </table>
+            </div>
+        </div>
+    </div>
+</section>
+
+    </body>
+
+    <!-- Petit CopyRight Inutile Donc Indispensable ! -->
+    <footer class=\"bg-black small text-center text-white-50\">
+        <div class=\"container\">
+            Copyright &copy; Dylan Soton & Victor Védie
+        </div>
+    </footer>
+
+    <!-- bootstrap.min & JavaScript -->
+    <script src=\"/js/bootstrap.min.bundle.js\"></script>
+    <script src=\"/js/bootstrap.min.js\"></script>
+
+    <script src=\"js/accueil.js\"></script>
+
+    </html>
+
 ";
         
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
@@ -139,45 +190,94 @@ class __TwigTemplate_6c3c2f7ea93458d7b3b5b73432469264cce484c1c4fa0ec4ee5548d94e2
 
     public function getDebugInfo()
     {
-        return array (  119 => 33,  114 => 31,  109 => 29,  101 => 24,  94 => 20,  87 => 16,  80 => 12,  72 => 6,  63 => 5,  45 => 3,  15 => 1,);
+        return array (  145 => 57,  138 => 53,  131 => 49,  124 => 45,  108 => 32,  101 => 28,  96 => 26,  72 => 4,  63 => 3,  45 => 2,  15 => 1,);
     }
 
     public function getSourceContext()
     {
         return new Twig_Source("{% extends 'base.html.twig' %}
-
 {% block title %}Etape{% endblock %}
+{% block main %}
+<html lang=\"fr\">
 
-{% block body %}
-    <h1>Etape</h1>
+<head>
 
-    <table class=\"table\">
-        <tbody>
-            <tr>
-                <th>Id</th>
-                <td>{{ etape.id }}</td>
-            </tr>
-            <tr>
-                <th>NumeroEtape</th>
-                <td>{{ etape.numeroEtape }}</td>
-            </tr>
-            <tr>
-                <th>VilleEtape</th>
-                <td>{{ etape.villeEtape }}</td>
-            </tr>
-            <tr>
-                <th>NombreJours</th>
-                <td>{{ etape.nombreJours }}</td>
-            </tr>
-        </tbody>
-    </table>
+    <meta charset=\"utf-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
+    <meta name=\"Auteur\" content=\"Dylan Soton & Victor Védie\">
 
-    <a href=\"{{ path('admin_etape_index') }}\">back to list</a>
+    <!-- Quelques polices d'écritures plus jolies :) -->
+    <link href=\"/css/bootstrap.min.css\" rel=\"stylesheet\">
+    <link href=\"https://fonts.googleapis.com/css?family=Varela+Round\" rel=\"stylesheet\">
+    <link href=\"https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i\" rel=\"stylesheet\">
 
-    <a href=\"{{ path('admin_etape_edit', {'id': etape.id}) }}\">edit</a>
+    <!-- Un Css modifié :) -->
+    <link href=\"../../css/circuit.css\" rel=\"stylesheet\">
 
+</head>
+<body id=\"page-top\">
+
+<!-- NavBar Menu -->
+<nav class=\"navbar navbar-expand-lg navbar-light fixed-top\" id=\"mainNav\">
+    <div class=\"container\">
+        <a href=\"{{ path('admin_etape_index') }}\">back to list</a>
+
+        <a href=\"{{ path('admin_etape_edit', {'id': etape.id}) }}\">edit</a>
+
+
+    </div>
     {{ include('back/etape/_delete_form.html.twig') }}
-{% endblock %}
+</nav>
+
+<section id=\"container-table\" class=\"about-section text-center\">
+    <div class=\"container\">
+        <div class=\"row\">
+            <div class=\"col-lg-10 mx-auto\">
+                    <h1>Etape</h1>
+
+                    <table class=\"table\">
+                        <tbody>
+                            <tr>
+                                <th>Id</th>
+                                <td>{{ etape.id }}</td>
+                            </tr>
+                            <tr>
+                                <th>NumeroEtape</th>
+                                <td>{{ etape.numeroEtape }}</td>
+                            </tr>
+                            <tr>
+                                <th>VilleEtape</th>
+                                <td>{{ etape.villeEtape }}</td>
+                            </tr>
+                            <tr>
+                                <th>NombreJours</th>
+                                <td>{{ etape.nombreJours }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+            </div>
+        </div>
+    </div>
+</section>
+
+    </body>
+
+    <!-- Petit CopyRight Inutile Donc Indispensable ! -->
+    <footer class=\"bg-black small text-center text-white-50\">
+        <div class=\"container\">
+            Copyright &copy; Dylan Soton & Victor Védie
+        </div>
+    </footer>
+
+    <!-- bootstrap.min & JavaScript -->
+    <script src=\"/js/bootstrap.min.bundle.js\"></script>
+    <script src=\"/js/bootstrap.min.js\"></script>
+
+    <script src=\"js/accueil.js\"></script>
+
+    </html>
+
+{% endblock %} {# main #}
 ", "back/etape/show.html.twig", "/home/victor/Documents/csc4101/AgenceVoyageTSP/templates/back/etape/show.html.twig");
     }
 }
