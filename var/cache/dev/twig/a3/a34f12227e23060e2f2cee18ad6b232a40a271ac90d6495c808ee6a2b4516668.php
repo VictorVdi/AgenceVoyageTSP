66947eb5b26c85f17b2ce6b32909d53721d411fe
VictorVdi <victor.vedie@telecom-sudparis.eu>
2018-11-27 16:38:43 +0100

    Toujours plus de CSS !

--- var/cache/dev/twig/a3/a34f12227e23060e2f2cee18ad6b232a40a271ac90d6495c808ee6a2b4516668.php
+++ var/cache/dev/twig/a3/a34f12227e23060e2f2cee18ad6b232a40a271ac90d6495c808ee6a2b4516668.php
@@ -14,7 +14,7 @@ class __TwigTemplate_a346d5986131055b5a1eb02398fe16404744bfa98463687cfe6298cbdde
         // line 1
         $this->parent = $this->loadTemplate("base.html.twig", "front/circuit_show.html.twig", 1);
         $this->blocks = array(
-            'body' => array($this, 'block_body'),
+            'main' => array($this, 'block_main'),
         );
     }
 
@@ -41,13 +41,13 @@ class __TwigTemplate_a346d5986131055b5a1eb02398fe16404744bfa98463687cfe6298cbdde
     }
 
     // line 3
-    public function block_body($context, array $blocks = array())
+    public function block_main($context, array $blocks = array())
     {
         $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
-        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "main"));
 
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
-        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "main"));
 
         // line 4
         echo "<html lang=\"fr\">
@@ -58,18 +58,13 @@ class __TwigTemplate_a346d5986131055b5a1eb02398fe16404744bfa98463687cfe6298cbdde
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
     <meta name=\"Auteur\" content=\"Dylan Soton & Victor Védie\">
 
-    <title>La Boule Magique</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href=\"/css/bootstrap.min.css\" rel=\"stylesheet\">
-
     <!-- Quelques polices d'écritures plus jolies :) -->
     <link href=\"/css/bootstrap.min.css\" rel=\"stylesheet\">
     <link href=\"https://fonts.googleapis.com/css?family=Varela+Round\" rel=\"stylesheet\">
     <link href=\"https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i\" rel=\"stylesheet\">
 
     <!-- Un Css modifié :) -->
-    <link href=\"css/accueil.css\" rel=\"stylesheet\">
+    <link href=\"../css/circuit.css\" rel=\"stylesheet\">
 
 </head>
 <body id=\"page-top\">
@@ -78,36 +73,31 @@ class __TwigTemplate_a346d5986131055b5a1eb02398fe16404744bfa98463687cfe6298cbdde
 <nav class=\"navbar navbar-expand-lg navbar-light fixed-top\" id=\"mainNav\">
     <div class=\"container\">
         <a class=\"navbar-brand js-scroll-trigger\" href=\"#page-top\">Circuit ";
-        // line 31
-        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["circuit"]) || array_key_exists("circuit", $context) ? $context["circuit"] : (function () { throw new Twig_Error_Runtime('Variable "circuit" does not exist.', 31, $this->source); })()), "description", array()), "html", null, true);
+        // line 26
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["circuit"]) || array_key_exists("circuit", $context) ? $context["circuit"] : (function () { throw new Twig_Error_Runtime('Variable "circuit" does not exist.', 26, $this->source); })()), "description", array()), "html", null, true);
         echo "</a>
         <div class=\"collapse navbar-collapse\" id=\"navbarResponsive\">
             <ul class=\"navbar-nav ml-auto\">
                 <li class=\"nav-item\">
-                    <a class=\"nav-link js-scroll-trigger\" href=\"#nouscontacter\">Nous contacter</a>
-                </li>
-            </ul>
-            <ul class=\"navbar-nav ml-auto\">
-                <li class=\"nav-item\">
                     <a class=\"nav-link js-scroll-trigger\" href=\"";
-        // line 40
-        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["circuit"]) || array_key_exists("circuit", $context) ? $context["circuit"] : (function () { throw new Twig_Error_Runtime('Variable "circuit" does not exist.', 40, $this->source); })()), "id", array()), "html", null, true);
-        echo "\">id</a>
+        // line 30
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["circuit"]) || array_key_exists("circuit", $context) ? $context["circuit"] : (function () { throw new Twig_Error_Runtime('Variable "circuit" does not exist.', 30, $this->source); })()), "id", array()), "html", null, true);
+        echo "\"></a>
                 </li>
             </ul>
 
         </div>
         <a href=\"";
-        // line 45
+        // line 35
         echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("front");
         echo "\">Liste des circuits</a>
     </div>
 </nav>
 
-<section id=\"container_table\" class=\"about-section text-center\">
+<section id=\"container-table\" class=\"about-section text-center\">
     <div class=\"container\">
         <div class=\"row\">
-            <div class=\"col-lg-8 mx-auto\">
+            <div class=\"col-lg-10 mx-auto\">
                 <h2 class=\"text-white mb-4\">Le circuit </h2>
                 <table class=\"table\">
                     <tr>
@@ -116,22 +106,22 @@ class __TwigTemplate_a346d5986131055b5a1eb02398fe16404744bfa98463687cfe6298cbdde
                         <th scope=\"col\">Durée</th>
                     </tr>
                 ";
-        // line 60
+        // line 50
         $context['_parent'] = $context;
-        $context['_seq'] = twig_ensure_traversable(twig_get_attribute($this->env, $this->source, (isset($context["circuit"]) || array_key_exists("circuit", $context) ? $context["circuit"] : (function () { throw new Twig_Error_Runtime('Variable "circuit" does not exist.', 60, $this->source); })()), "etapes", array()));
+        $context['_seq'] = twig_ensure_traversable(twig_get_attribute($this->env, $this->source, (isset($context["circuit"]) || array_key_exists("circuit", $context) ? $context["circuit"] : (function () { throw new Twig_Error_Runtime('Variable "circuit" does not exist.', 50, $this->source); })()), "etapes", array()));
         foreach ($context['_seq'] as $context["_key"] => $context["etape"]) {
-            // line 61
+            // line 51
             echo "                    <tr>
                         <td>";
-            // line 62
+            // line 52
             echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["etape"], "numeroEtape", array()), "html", null, true);
             echo "</td>
                         <td>";
-            // line 63
+            // line 53
             echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["etape"], "getVilleEtape", array(), "method"), "html", null, true);
             echo "</td>
                         <td>";
-            // line 64
+            // line 54
             echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["etape"], "getNombreJours", array(), "method"), "html", null, true);
             echo "</td>
                     </tr>
@@ -140,9 +130,9 @@ class __TwigTemplate_a346d5986131055b5a1eb02398fe16404744bfa98463687cfe6298cbdde
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['etape'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 66
+        // line 56
         echo " ";
-        // line 67
+        // line 57
         echo "                </table>
 
             </div>
@@ -151,6 +141,20 @@ class __TwigTemplate_a346d5986131055b5a1eb02398fe16404744bfa98463687cfe6298cbdde
 </section>
 
 </body>
+
+<!-- Petit CopyRight Inutile Donc Indispensable ! -->
+<footer class=\"bg-black small text-center text-white-50\">
+    <div class=\"container\">
+        Copyright &copy; Dylan Soton & Victor Védie
+    </div>
+</footer>
+
+<!-- bootstrap.min & JavaScript -->
+<script src=\"/js/bootstrap.min.bundle.js\"></script>
+<script src=\"/js/bootstrap.min.js\"></script>
+
+<script src=\"js/accueil.js\"></script>
+
 </html>
 
 ";
@@ -174,14 +178,14 @@ class __TwigTemplate_a346d5986131055b5a1eb02398fe16404744bfa98463687cfe6298cbdde
 
     public function getDebugInfo()
     {
-        return array (  146 => 67,  144 => 66,  135 => 64,  131 => 63,  127 => 62,  124 => 61,  120 => 60,  102 => 45,  94 => 40,  82 => 31,  53 => 4,  44 => 3,  15 => 1,);
+        return array (  136 => 57,  134 => 56,  125 => 54,  121 => 53,  117 => 52,  114 => 51,  110 => 50,  92 => 35,  84 => 30,  77 => 26,  53 => 4,  44 => 3,  15 => 1,);
     }
 
     public function getSourceContext()
     {
         return new Twig_Source("{% extends 'base.html.twig' %}
 
-{% block body %}
+{% block main %}
 <html lang=\"fr\">
 
 <head>
@@ -190,18 +194,13 @@ class __TwigTemplate_a346d5986131055b5a1eb02398fe16404744bfa98463687cfe6298cbdde
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
     <meta name=\"Auteur\" content=\"Dylan Soton & Victor Védie\">
 
-    <title>La Boule Magique</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href=\"/css/bootstrap.min.css\" rel=\"stylesheet\">
-
     <!-- Quelques polices d'écritures plus jolies :) -->
     <link href=\"/css/bootstrap.min.css\" rel=\"stylesheet\">
     <link href=\"https://fonts.googleapis.com/css?family=Varela+Round\" rel=\"stylesheet\">
     <link href=\"https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i\" rel=\"stylesheet\">
 
     <!-- Un Css modifié :) -->
-    <link href=\"css/accueil.css\" rel=\"stylesheet\">
+    <link href=\"../css/circuit.css\" rel=\"stylesheet\">
 
 </head>
 <body id=\"page-top\">
@@ -213,12 +212,7 @@ class __TwigTemplate_a346d5986131055b5a1eb02398fe16404744bfa98463687cfe6298cbdde
         <div class=\"collapse navbar-collapse\" id=\"navbarResponsive\">
             <ul class=\"navbar-nav ml-auto\">
                 <li class=\"nav-item\">
-                    <a class=\"nav-link js-scroll-trigger\" href=\"#nouscontacter\">Nous contacter</a>
-                </li>
-            </ul>
-            <ul class=\"navbar-nav ml-auto\">
-                <li class=\"nav-item\">
-                    <a class=\"nav-link js-scroll-trigger\" href=\"{{ circuit.id }}\">id</a>
+                    <a class=\"nav-link js-scroll-trigger\" href=\"{{ circuit.id }}\"></a>
                 </li>
             </ul>
 
@@ -227,10 +221,10 @@ class __TwigTemplate_a346d5986131055b5a1eb02398fe16404744bfa98463687cfe6298cbdde
     </div>
 </nav>
 
-<section id=\"container_table\" class=\"about-section text-center\">
+<section id=\"container-table\" class=\"about-section text-center\">
     <div class=\"container\">
         <div class=\"row\">
-            <div class=\"col-lg-8 mx-auto\">
+            <div class=\"col-lg-10 mx-auto\">
                 <h2 class=\"text-white mb-4\">Le circuit </h2>
                 <table class=\"table\">
                     <tr>
@@ -253,9 +247,23 @@ class __TwigTemplate_a346d5986131055b5a1eb02398fe16404744bfa98463687cfe6298cbdde
 </section>
 
 </body>
+
+<!-- Petit CopyRight Inutile Donc Indispensable ! -->
+<footer class=\"bg-black small text-center text-white-50\">
+    <div class=\"container\">
+        Copyright &copy; Dylan Soton & Victor Védie
+    </div>
+</footer>
+
+<!-- bootstrap.min & JavaScript -->
+<script src=\"/js/bootstrap.min.bundle.js\"></script>
+<script src=\"/js/bootstrap.min.js\"></script>
+
+<script src=\"js/accueil.js\"></script>
+
 </html>
 
-{% endblock %} {# body #}
+{% endblock %} {# main #}
 ", "front/circuit_show.html.twig", "/home/victor/Documents/csc4101/AgenceVoyageTSP/templates/front/circuit_show.html.twig");
     }
 }
